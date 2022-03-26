10255072ece3eb977d91358c79fad7ed0500f208
pavanreddy <pavanreddygottimukkula@gmailcom>
2022-03-25 20:37:33 -0500

    added map

--- orangepark/src/App.js
+++ orangepark/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { render } from "react-dom";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Map = ReactMapboxGl({
+  preserveDrawingBuffer: true,
+  accessToken:
+    "pk.eyJ1IjoibWF5b2ppY2giLCJhIjoiY2pla3Q3MzVvMWRoYTJybnVyMndxM2hmdCJ9.nWZlYcpKaMqz6m7xTsnJGA"
+});
+class BaseMap extends React.Component {
+  render() {
+    return (
+      <><h1>Orange Park</h1>
+      <Map
+      // eslint-disable-next-line
+        style={"mapbox://styles/mapbox/streets-v8"}
+        zoom={[10]}
+        containerStyle={{
+          height: "60vh",
+          width: "100vw"
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[101.8224, 2.955139]} />
+        </Layer>
+      </Map></>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BaseMap ref={el => (this.componentRef = el)} />
+      </div>
+    );
+  }
 }
 
 export default App;
